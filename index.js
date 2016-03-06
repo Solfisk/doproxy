0bd1b251e7d5527ead9a943e65a6293b8e6137a8
Michael Zedeler <michael@zedeler.dk>
2016-03-06 16:29:46 +0100

    Now it works.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const JSONStream = require('JSONStream'),
       config = require('config'),
       https = require('https'),
       proxy = require('http-proxy').createProxyServer({}),
+      DigitalOcean = require('do-wrapper'),
       digitalOceanApiAddress = 'api.digitalocean.com';
 
 module.exports = () => {
@@ -21,7 +22,8 @@ module.exports = () => {
       const buffers = require('stream-buffers');
       const writable = new buffers.WritableStreamBuffer();
       const fromToken = ((req.headers.authorization || '').match(/Bearer (.+)/) || [])[1];
-      const toToken = tokenMap[fromToken];
+      const name = tokenMap[fromToken];
+      const toToken = (config.get('tokens')[tokenMap[fromToken]] || {}).to;
 
       if(!toToken) {
         res.statusCode = 403;
@@ -33,44 +35,89 @@ module.exports = () => {
       req.pipe(writable);
       
       req.on('end', () => {
-        console.log('end:');
         const requestObject = JSON.parse(writable.getContents());
     
-        function validate(obj) {
-          return obj;
-        }
-        
-        try {
-          let newRequest = validate(requestObject);
-          
-          console.log(newRequest);
-          const readable = new buffers.ReadableStreamBuffer();
-
-          for(let method of ['on', 'read', 'pipe', 'isPaused', 'pause', 'resume', 'setEncoding', 'unpipe', 'unshift', 'wrap']) {
-            req[method] = readable[method] ? readable[method].bind(readable) : undefined;
+        function validate(req, obj, cb) {
+          const pathname = require('url').parse(req.url).pathname,
+                match = pathname.match(/\/droplets(\/(.+$))?/),
+                droplet = (match || [])[1];
+          if(!match) {
+            cb(null, obj);
+            return;
           }
-
-          const newRequestBuffer = new Buffer(JSON.stringify(newRequest));
-          
-          readable.put(newRequestBuffer);
-          readable.stop();
-
-          proxy.web(req, res, {
-            target: 'https://' + digitalOceanApiAddress,
-            agent: https.globalAgent,
-            headers: {
-              'content-length': newRequestBuffer.length,
-              host: digitalOceanApiAddress
+          console.log(req.method + ' ' + req.url + ' ' + toToken);
+          if(obj && obj.name !== name) {
+            cb('Name must be ' + name);
+            return;
+          }
+          const api = new DigitalOcean(toToken);
+          api.dropletsGetAll('*', (err, res, body) => {
+            if(err) {
+              cb(err);
+              return;
+            }
+            for(let droplet of body.droplets) {
+              console.log(droplet.id + ': ' + droplet.name);
+              if(obj && obj.name === droplet.name) {
+                cb('Droplet with name ' + name + ' already exists');
+                return;
+              }
+              if(droplet && droplet.id && droplet.name !== name) {
+                cb('The droplet ' + droplet.id + ' is protected');
+                return;
+              }
+            }
+            if(req.method === 'POST') {
+              api.sizesGetAll('*', (err, res, body) => {
+                console.log('Available sizes: ');
+                for(let size of body.sizes) {
+                  console.log(size.slug + ': ' + size.price_monthly);
+                  if(obj.size === size.slug) {
+                    if(size.price_monthly <= 20) {
+                      cb(null, obj);
+                      return;
+                    } else {
+                      cb('Pricing of size ' + obj.size + ' is above $20');
+                      return;
+                    }
+                  }
+                }
+                cb('Unidentified size: ' + obj.size);
+                return;
+              });
+            } else {
+              cb(null, obj);
             }
           });
-          
-        } catch(e) {
-          res.statusCode = 403;
-          res.statusMessage = e.toString();
-          console.log(e.toString());
-          res.end();
         }
+        
+        validate(req, requestObject, (err, newRequest) => {
+          if(!err) {
+            const readable = new buffers.ReadableStreamBuffer();
 
+            for(let method of ['on', 'read', 'pipe', 'isPaused', 'pause', 'resume', 'setEncoding', 'unpipe', 'unshift', 'wrap']) {
+              req[method] = readable[method] ? readable[method].bind(readable) : undefined;
+            }
+
+            const newRequestBuffer = new Buffer(JSON.stringify(newRequest));
+          
+            readable.put(newRequestBuffer);
+            readable.stop();
+            proxy.web(req, res, {
+              target: 'https://' + digitalOceanApiAddress,
+              agent: https.globalAgent,
+              headers: {
+                'content-length': newRequestBuffer.length,
+                authorization: 'Bearer ' + toToken,
+                host: digitalOceanApiAddress
+              }
+            });
+          } else {
+            res.statusCode = 403;
+            res.statusMessage = err;
+            res.end();
+          }
+        });
       });
     }
   );
---
+++ server.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+'use strict';
+
+const Proxy = require('./index.js');
+const proxy = Proxy('');
+proxy.listen(3000);
