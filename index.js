cc1a80686980fff365993a39ef968d0e83705565
Michael Zedeler <michael@zedeler.dk>
2016-03-06 15:02:42 +0100

    Next try.

--- index.js
+++ index.js
@@ -20,33 +20,56 @@ module.exports = () => {
           encoding;
       const buffers = require('stream-buffers');
       const writable = new buffers.WritableStreamBuffer();
+      const fromToken = ((req.headers.authorization || '').match(/Bearer (.+)/) || [])[1];
+      const toToken = tokenMap[fromToken];
 
+      if(!toToken) {
+        res.statusCode = 403;
+        res.statusMessage = 'Unrecognized authorization';
+        res.end();
+        return;
+      }
+      
       req.pipe(writable);
       
       req.on('end', () => {
         console.log('end:');
-//        console.log(writable.getContentsAsString('utf-8'));
-        let buffer = writable.getContents();
-        console.log(buffer);
-        console.log(buffer.toString());
-        
-        const readable = new buffers.ReadableStreamBuffer();
-
-        for(let method of ['on', 'read', 'pipe', 'isPaused', 'pause', 'resume', 'setEncoding', 'unpipe', 'unshift', 'wrap']) {
-          req[method] = readable[method] ? readable[method].bind(readable) : undefined;
+        const requestObject = JSON.parse(writable.getContents());
+    
+        function validate(obj) {
+          return obj;
         }
+        
+        try {
+          let newRequest = validate(requestObject);
+          
+          console.log(newRequest);
+          const readable = new buffers.ReadableStreamBuffer();
 
-        readable.put(buffer);
-        readable.stop();
-
-        proxy.web(req, res, {
-          target: 'https://' + digitalOceanApiAddress,
-          agent: https.globalAgent,
-          headers: {
-            'content-length': buffer.length,
-            host: digitalOceanApiAddress
+          for(let method of ['on', 'read', 'pipe', 'isPaused', 'pause', 'resume', 'setEncoding', 'unpipe', 'unshift', 'wrap']) {
+            req[method] = readable[method] ? readable[method].bind(readable) : undefined;
           }
-        });
+
+          const newRequestBuffer = new Buffer(JSON.stringify(newRequest));
+          
+          readable.put(newRequestBuffer);
+          readable.stop();
+
+          proxy.web(req, res, {
+            target: 'https://' + digitalOceanApiAddress,
+            agent: https.globalAgent,
+            headers: {
+              'content-length': newRequestBuffer.length,
+              host: digitalOceanApiAddress
+            }
+          });
+          
+        } catch(e) {
+          res.statusCode = 403;
+          res.statusMessage = e.toString();
+          console.log(e.toString());
+          res.end();
+        }
 
       });
     }
